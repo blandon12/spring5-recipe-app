92ee38c192a10c0eb87d9f33f00a8c9b16c9f696
Chunxin Xu <chunxin.xu@worldfirst.com>
2018-04-07 14:32:04 +0100

    create integration test

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -1,32 +1,76 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
+import guru.springframework.services.RecipeServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+
+import java.util.Set;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+@RunWith(MockitoJUnitRunner.class)
 public class RecipeControllerTest {
 
     @Mock
-    RecipeService recipeService;
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    Model model;
 
     RecipeController recipeController;
 
     @Before
     public void setUp() throws Exception {
+//        MockitoAnnotations.initMocks(this);
         recipeController = new RecipeController(recipeService);
     }
 
     @Test
-    public void showById() {
+    public void mockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+    }
+
+    @Test
+    public void showByIdTest() {
 
         // given
+        final Long RECIPE_ID = new Long(1);
+        Recipe recipe = new Recipe();
+        recipe.setId(RECIPE_ID);
+        when(recipeService.findById(RECIPE_ID)).thenReturn(recipe);
+        ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
 
         // when
+        String template = recipeController.showById("1", model);
 
         // then
+        assertEquals("recipe/show", template);
+        verify(recipeService, times(1)).findById(RECIPE_ID);
+        verify(model, times(1)).addAttribute(eq("recipe"), argumentCaptor.capture());
+
+        Recipe recipeInController = argumentCaptor.getValue();
+        assertEquals(recipe, recipeInController);
 
     }
 }
